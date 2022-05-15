aa8e4e86820f3de990807dc2ddc9aa91679d6d9e
fog_grass <postal6000@gmail.com>
2022-05-15 18:38:14 -0400

    Board,figures,starting generation,cell selection

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,15 +4,17 @@ import { Colors } from '../models/Colors';
 
 interface CellProps {
   cell: Cell;
-  color: Colors;
+  selected: boolean;
+  click: (cell: Cell) => void; 
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
     <div
-      className={['cell', cell.color].join(' ')}
+      className={['cell', cell.color, selected ? "selected" : ""].join(' ')}
+      onClick={() => click(cell)}
     >
-      
+      {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
     </div>
   );
 };
