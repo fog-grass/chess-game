bddc035e4418203080a1da61ccd393dd526374c4
fog_grass <postal6000@gmail.com>
2022-09-11 23:43:12 -0400

    idk :P

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Board } from '../models/Board';
 import { Cell } from '../models/Cell';
 import CellComponent from './CellComponent';
@@ -12,9 +12,22 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
   const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
   
   function click(cell: Cell) {
-    setSelectedCell(cell);
+    if(cell.figure !== null) {
+      setSelectedCell(cell);
+    };
   }
 
+  useEffect(() => highlightCells(), [selectedCell])
+
+  function highlightCells() {
+    board.highlightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
   return (
     <div className='board'>
       {board.cells.map((row, index) => 
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -11,9 +11,15 @@ interface CellProps {
 const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
     <div
-      className={['cell', cell.color, selected ? "selected" : ""].join(' ')}
+      className={[
+        'cell', 
+        cell.color, 
+        selected ? "selected" : "",
+        cell.available && cell.figure ? "hit" : ""
+      ].join(' ')}
       onClick={() => click(cell)}
     >
+      {cell.available && !cell.figure && <div className={"available"} />}
       {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
     </div>
   );
